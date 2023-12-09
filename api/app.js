801446a4b8d79e2f7d7f9b61aacb45d754f587ed
Pablo <pablito@me.com>
2023-12-09 15:06:04 +0000

    Added context statements to Login submit content text

--- api/app.js
+++ api/app.js
@@ -34,6 +34,15 @@ app.options('/posts/:id', (req, res) => {
   res.sendStatus(200); // Send a 200 OK response
 });
 
+// Below is code that made it possible to pass CORS policy when checking server status
+app.options('/health', (req, res) => {
+  // Set the appropriate CORS headers for the preflight request
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.sendStatus(200); // Send a 200 OK response
+});
+
 // Added CORS policy
 app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
   // set the response header to allow all origins
--- frontend/src/components/landing/LandingPage.js
+++ frontend/src/components/landing/LandingPage.js
@@ -6,6 +6,7 @@ const LogInForm = ({ navigate }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [isServerUp, setIsServerUp] = useState(false);
+  const [counter, setCounter] = useState(45);
   window.localStorage.setItem("app-route", "login")
 
   useEffect(() => {
@@ -28,7 +29,8 @@ const LogInForm = ({ navigate }) => {
     // Optionally, you can set up an interval to periodically check the server status
     const intervalId = setInterval(() => {
       checkServerStatus(); // check server status every x seconds
-    }, 2000);
+      setCounter(counter => counter > 0 ? counter - 1 : counter)
+    }, 1000);
 
     if (isServerUp) {
       clearInterval(intervalId);
@@ -73,6 +75,7 @@ const LogInForm = ({ navigate }) => {
 
 
   return (
+    <>
     <div className='login-form'>
       <div className='title-container'>
         <h1 className='login-title'>Log in to Farcebook</h1>
@@ -83,13 +86,19 @@ const LogInForm = ({ navigate }) => {
         <label htmlFor="password">Password: </label>
         <input placeholder='Password' id="password" type='password' value={password} onChange={handlePasswordChange} /> <br />
         <p id='error-message'></p>
-        {isServerUp ?
-          (<input role='submit-button' id='submit' type="submit" value='Submit' />) :
-          (<input id='submit' type="" value='Waiting for server - Please be patient' />)}
-        {/* <input role='submit-button' id='submit' type="submit" value={isServerUp ? 'Submit' : 'Waiting for server'} /> */}
+          {(isServerUp || counter > 44) && <input role='submit-button' id='submit' type="submit" value='Submit' />}
+          {/* {(!isServerUp && counter > 44) && <input role='submit-button' id='submit' type="submit" value='' />}  */}
+          {(!isServerUp && counter <= 44) && <input role='submit-button' id='submit' type="submit" value='Waiting for server - Please be patient' />}
       </form>
-    
-    </div>
+      </div>
+
+      {(!isServerUp && counter < 43) && (<div className='login-form notice'>
+        <p id='server-coutdown'>The server is expected to be up in around {counter} seconds.</p>
+        <p>This App is currently deployed on a free tier of Render.com which is great but means the server spins down after 15 minutes of inactivity.</p>
+        <p>Please be patient while the server spins back up which may take around a minute.</p>
+        <p>Feel free to browse another tab while this is happening.</p>
+      </div>)}
+      </>
   )
 }
 
