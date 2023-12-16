9328c13fe71068db39b23692ad68ecf171a45c35
Pablo <pablito@me.com>
2023-12-16 13:09:32 +0000

    Moved CORS to be first middleware

--- api/app.js
+++ api/app.js
@@ -10,6 +10,21 @@ const usersRouter = require("./routes/users");
 
 const app = express();
 
+// Added CORS policy
+app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
+  // set the response header to allow all origins
+  console.log("CORS policy is being set");
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  // res.setHeader('Access-Control-Allow-Origin', 'https://farcebook-9uwa.onrender.com');
+  // res.setHeader('Access-Control-Allow-Origin', 'https://farcebook-9uwa.onrender.com/');
+  // set the response header to allow the following headers
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+  // set the response header to allow the following methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  // call the next function which will be executed in the middleware stack
+  next();
+});
+
 // setup for receiving JSON
 app.use(express.json())
 
@@ -25,14 +40,14 @@ app.use(express.static(path.join(__dirname, "public")));
 //   res.sendStatus(200); // Send a 200 OK response
 // });
 
-// Below is code that made it possible to pass CORS policy when creating a message
-app.options('/posts/:id', (req, res) => {
-  // Set the appropriate CORS headers for the preflight request
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.sendStatus(200); // Send a 200 OK response
-});
+// // Below is code that made it possible to pass CORS policy when creating a message
+// app.options('/posts/:id', (req, res) => {
+//   // Set the appropriate CORS headers for the preflight request
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//   res.sendStatus(200); // Send a 200 OK response
+// });
 
 // // Below is code that made it possible to pass CORS policy when checking server status
 // app.options('/health', (req, res) => {
@@ -43,20 +58,20 @@ app.options('/posts/:id', (req, res) => {
 //   res.sendStatus(200); // Send a 200 OK response
 // });
 
-// Added CORS policy
-app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
-  // set the response header to allow all origins
-  console.log("CORS policy is being set");
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
-  // res.setHeader('Access-Control-Allow-Origin', 'https://farcebook-9uwa.onrender.com');
-  // res.setHeader('Access-Control-Allow-Origin', 'https://farcebook-9uwa.onrender.com/');
-  // set the response header to allow the following headers
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
-  // set the response header to allow the following methods
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  // call the next function which will be executed in the middleware stack
-  next();
-});
+// // Added CORS policy
+// app.use((req, res, next) => { // call the use method, which adds a middleware function to the middleware stack
+//   // set the response header to allow all origins
+//   console.log("CORS policy is being set");
+//   res.setHeader('Access-Control-Allow-Origin', '*'); 
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://farcebook-9uwa.onrender.com');
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://farcebook-9uwa.onrender.com/');
+//   // set the response header to allow the following headers
+//   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'); 
+//   // set the response header to allow the following methods
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//   // call the next function which will be executed in the middleware stack
+//   next();
+// });
 
 // avatars is the URL path to access the avatars folder
 //display the images in the avatars folder
