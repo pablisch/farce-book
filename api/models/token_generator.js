a30fd9c46c22b09de9bc9473cde184b15f72d7a5
Pablo <pablito@me.com>
2024-01-15 17:44:48 +0000

    Refactored db url with a view to making this repo work locally or deployed from the same branch

--- api/models/token_generator.js
+++ api/models/token_generator.js
@@ -1,4 +1,5 @@
 const JWT = require("jsonwebtoken");
+require('dotenv').config()
 const secret = process.env.JWT_SECRET;
 
 class TokenGenerator {
@@ -7,8 +8,8 @@ class TokenGenerator {
       user_id: user_id,
       iat: Math.floor(Date.now() / 1000),
       
-      // Set the JWT token to expire in 10 minutes
-      exp: Math.floor(Date.now() / 1000) + (30 * 60) // REAL LINE
+      // Set the JWT token to expire in 30 minutes
+      exp: Math.floor(Date.now() / 1000) + (30 * 60)
     }, secret);
   }
 }
