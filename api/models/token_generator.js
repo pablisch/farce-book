47cbec8b4616f68b3e7267048f8f4f91f7e9399d
Pablo <pablito@me.com>
2024-01-15 17:12:15 +0000

    Set tokens valid for 30 mins

--- api/models/token_generator.js
+++ api/models/token_generator.js
@@ -8,8 +8,7 @@ class TokenGenerator {
       iat: Math.floor(Date.now() / 1000),
       
       // Set the JWT token to expire in 10 minutes
-      // exp: Math.floor(Date.now() / 1000) + (10 * 60) // REAL LINE
-      exp: Math.floor(Date.now() / 1000) + (100 * 60) // EXTENDED TOKEN LINE
+      exp: Math.floor(Date.now() / 1000) + (30 * 60) // REAL LINE
     }, secret);
   }
 }
