5e61f2d9af7106401ac2a1cf41d522950ea967e1
Pablo <pablito@me.com>
2024-02-22 22:16:42 +0000

    Edit to baseUrl conditional value.

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -1,3 +1,3 @@
-export default process.env.NODE_ENV === 'deploy'
+export default process.env.NODE_ENV === 'deploy' || process.env.NODE_ENV === 'production'
   ? 'https://farcebook-backend.onrender.com'
   : 'http://localhost:8080';
